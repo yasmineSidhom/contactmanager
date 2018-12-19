2aec56dbb5f4e414d54786e23aec233212543527
yasmineSidhom <yasmine.sidhom@gmail.com>
2018-12-19 14:14:35 +0100

    Updates

---
+++ precache-manifest.650e5d656167a4f461bd4ded20b7d951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "36869238b2e77bb1c9a6",
+    "url": "/contactmanager/static/js/main.36869238.chunk.js"
+  },
+  {
+    "revision": "eb537a4917dd19138541",
+    "url": "/contactmanager/static/js/1.eb537a49.chunk.js"
+  },
+  {
+    "revision": "36869238b2e77bb1c9a6",
+    "url": "/contactmanager/static/css/main.4ab803a3.chunk.css"
+  },
+  {
+    "revision": "eb537a4917dd19138541",
+    "url": "/contactmanager/static/css/1.63f1a2d2.chunk.css"
+  },
+  {
+    "revision": "ae6aa962593fef6720be7437dd2548fd",
+    "url": "/contactmanager/index.html"
+  }
+];
